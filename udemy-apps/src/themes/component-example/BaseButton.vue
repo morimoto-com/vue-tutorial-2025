fb054d43c24aaaa4711b7930da85a07e8f332e54
morimoto <morimoto820@yahoo.co.jp>
2025-07-18 10:04:00 +0900

    conponentの親子間属性とスタイルの学習

---
+++ udemy-apps/src/themes/component-example/BaseButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <!-- 親コンポーネントで指定された属性がオブジェクトとして連携される -->
+    <p>{{ $attrs }}</p>
+    <button>BaseButton</button>
+  </div>
+</template>
--- udemy-apps/src/themes/component-example/ComponentExample.vue
+++ udemy-apps/src/themes/component-example/ComponentExample.vue
@@ -3,10 +3,29 @@
   <!-- ケバブケースで呼び出しすることも可能 -->
   <Count-Up />
   <CountUp />
+  <!-- idを付与することで、子コンポーネントの最上位タグに付与される -->
+  <!-- 子コンポーネントにもidが付与されている場合は、親コンポーネントのidが付与される -->
+  <!-- スタイルはどちらも適用される -->
+  <BaseButton id="base-button" />
+  <BaseButton v-if="true" id="base-button" />
+  <!-- v-showは子コンポーネントの最上位タグが１つのものでないと動作しない（display属性が付与されるため） -->
+  <BaseButton v-show="true" id="base-button" />
 </template>
 <script setup>
 // vue.jsではパスカルケースでコンポーネントを命名することを推奨されている
 // また、コンポーネント名は2単語以上にすることが求められる
+import BaseButton from './BaseButton.vue'
 import CountUp from './CountUp.vue'
 </script>
-<style></style>
+<!-- scopedを指定することでこのコンポーネントにのみスタイルを適用することを限定する。
+指定がなければ子コンポーネントでも使用できてしまい可読性が下がる -->
+<style scoped>
+.red {
+  color: red;
+}
+</style>
+<style>
+/* body {
+  background-color: rgb(65, 65, 38);
+} */
+</style>
