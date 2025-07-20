2eb14a822e843d76de92c25b72249fd7982073e3
morishin <morimoto820@yahoo.co.jp>
2025-07-21 07:17:32 +0900

    propsの学習２

--- udemy-apps/src/themes/props-example/PropsExample.vue
+++ udemy-apps/src/themes/props-example/PropsExample.vue
@@ -1,11 +1,14 @@
 <template>
   <!-- definePropsの属性は子コンポーネントへは継承されない -->
-  <ChildComponent :foo="count" bar="Hello"/>
+  <ChildComponent :foo="count" bar="Hello" />
+  <!-- 子コンポーネントでキャメルケースで定義された属性はケバブケースで指定することが推奨される。（HTMLの属性値は大文字小文字判別ないため、類似属性との混在が懸念される。） -->
+  <ValidationChild :foo-count="count" bar="Hello" />
   <button @click="count++">button</button>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import ChildComponent from './PropsExampleChild.vue';
+import { ref } from 'vue'
+import ChildComponent from './PropsExampleChild.vue'
+import ValidationChild from './PropsValidationChildExample.vue'
 const count = ref(0)
 </script>
--- udemy-apps/src/themes/props-example/PropsValidationChildExample.vue
+++ udemy-apps/src/themes/props-example/PropsValidationChildExample.vue
@@ -1,12 +1,28 @@
 <template>
-  <p>count:{{ foo }}</p>
+  <p>count:{{ fooCount }}</p>
 </template>
 <script setup>
 defineProps({
-  foo: {
+  fooCount: {
     type: Number,
-    required: true,
-    default: 100, // 指定しなければ'undefined'が設定される
+    // タイプには以下のような指定が可能
+    // type: [Number, String, Boolean, Array, Object, Function, Symbol, Date, CustomClass],
+    // required: true,
+    // defaultを指定しなければ'undefined'が設定される
+    default: 0,
+    // デフォルトにオブジェクトまたは配列を指定したい場合は、functionでデフォルト定義する
+    // default: function() {
+    //   return {}
+    // },
+    validator: function (value) {
+      // ↓省略して記載もできる
+      // valudator(value) {
+      return value === 0 || value === 1
+    },
+  },
+  bar: {
+    type: String,
+    default: 'test',
   },
 })
 </script>
