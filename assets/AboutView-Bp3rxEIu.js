d93cd5493e11e70b24fea2ae5f22328e8a60d7e3
morimoto-com <morimoto-com@users.noreply.github.com>
2025-08-07 07:26:00 +0000

    deploy: 37bdc1172c3063326f1e6d0a9f44116b7560b0f8

--- assets/AboutView-Bp3rxEIu.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,o as t}from"./index-DLkw8dXB.js";const c={},n={id:"about"};function r(s,_){return t(),o("h2",n,"About")}const f=e(c,[["render",r]]);export{f as default};
---
+++ assets/AboutView-BtIxySIp.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as t}from"./index-DT-jYI-D.js";const c={},n={id:"about"};function r(s,_){return t(),o("h2",n,"About")}const f=e(c,[["render",r]]);export{f as default};
